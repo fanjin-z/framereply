bb3c4cb1a6def5d3a1b5ae51204b90d41c0b56da
Fanjin Zeng <fjzeng@outlook.com>
2026-03-15 19:56:51 +0700

    feat: add initial project files and configuration for zeptly app

---
+++ zeptly/zeptlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  zeptlyApp.swift
+//  zeptly
+//
+//  Created by Fanjin Zeng on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct zeptlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
